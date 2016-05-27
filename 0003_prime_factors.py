3500ed81250ee9760661260aa50f2603ed6d3393
Craig Andrews <craigandrews@nhs.net>
2016-05-27 16:01:38 +0100

    Hack optimisation - not really working but ok

--- 0003_prime_factors.py
+++ 0003_prime_factors.py
@@ -19,13 +19,12 @@ def is_prime(x):
     return all(x % y for y in range(3, limit))
 
 
-def primes():
-    return (x for x in count(1) if is_prime(x))
+def reverse_primes(start):
+    return (x for x in range(start, 1, -1) if is_prime(x))
 
-
-def prime_factors(x):
+def highest_prime_factor(x):
     limit = int(sqrt(x) + 1)
-    return (y for y in takewhile(lambda z: z < limit, primes()) if not x % y)
+    return next(y for y in reverse_primes(limit) if not x % y)
 
 
-print max(prime_factors(600851475143))
+print highest_prime_factor(600851475143)
