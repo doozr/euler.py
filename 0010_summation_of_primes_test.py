7c55f59d7adc3d0ace637d68d120e68e789c96a0
Craig Andrews <craigandrews@nhs.net>
2016-05-31 13:12:37 +0100

    0010 summation of primes

---
+++ 0010_summation_of_primes_test.py
@@ -0,0 +1,20 @@
+"""
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+
+
+Answer:
+    142913828922
+"""
+
+
+from euler.prime import sieve
+
+
+def summation_of_primes(n):
+    return sum(sieve(n))
+
+
+def test_0010_summation_of_primes():
+    assert summation_of_primes(2000000) == 142913828922
--- euler/prime.py
+++ euler/prime.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from itertools import count
 
 
 def is_prime(x):
@@ -6,3 +7,17 @@ def is_prime(x):
         return False
     limit = int(sqrt(x) + 1)
     return all(x % y for y in range(3, limit))
+
+
+def primes():
+    return (x for x in count() if is_prime(x))
+
+
+def sieve(size):
+    marked = [0] * size
+    value = 3
+    yield 2
+    for value in (x for x in xrange(value, size, 2) if marked[x] == 0):
+        yield value
+        for i in xrange(value, size, value):
+            marked[i] = 1
