8bf94ef3a63042b7a3a677a7c8e47929b9f26855
Craig Andrews <craigandrews@nhs.net>
2016-05-31 08:10:08 +0100

    0005 smallest multiple

---
+++ 0005_smallest_multiple.py
@@ -0,0 +1,18 @@
+"""
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+
+Answer:
+    232792560
+"""
+
+# For each value that the accumulator does not already divide by,
+# multiply the accumulator by the lowest value that makes it divide
+# the old accumulator and the new value
+print reduce(
+    lambda acc, x: acc * next(y for y in range(1, x + 1) if (acc * y) % x == 0),
+    range(2, 21),
+    1
+)
