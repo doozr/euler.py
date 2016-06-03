c4538d1203fbec5d6aa3c9967f1985726bb8ecaf
Craig Andrews <craigandrews@nhs.net>
2016-06-03 08:56:22 +0100

    0067 maximum path sum 2

---
+++ 0067_maximum_path_sum_2_test.py
@@ -0,0 +1,49 @@
+"""
+By starting at the top of the triangle below and moving to adjacent numbers on the row below,
+the maximum total from top to bottom is 23.
+
+3
+7 4
+2 4 6
+8 5 9 3
+
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom in triangle.txt (right click and 'Save Link/Target As...'),
+a 15K text file containing a triangle with one-hundred rows.
+
+NOTE: This is a much more difficult version of Problem 18. It is not possible to try every route to
+solve this problem, as there are 299 altogether! If you could check one trillion (10**12) routes
+every second it would take over twenty billion years to check them all. There is an efficient
+algorithm to solve it. ;o)
+
+
+Answer:
+    7273
+"""
+
+
+from os import path
+from itertools import izip
+from euler.iter import window
+
+
+def load_triangle():
+    cwd = path.dirname(path.realpath(__file__))
+    with open(path.join(cwd, "files", "p067_triangle.txt")) as f:
+        return [[int(x.lstrip("0")) for x in l.split(" ")] for l in f]
+
+
+# Copied from 0018
+def maximum_path_sum(triangle):
+    iterator = reversed(triangle)
+    return next(reduce(
+        lambda a, xs: (x + max(w) for x, w in izip(xs, window(a, 2))),
+        iterator, next(iterator)
+    ))
+
+
+def test_0067_maximum_path_sum():
+    triangle = load_triangle()
+    assert maximum_path_sum(triangle) == 7273
+
