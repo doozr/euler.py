756dfc50b4d83b054c73629d21b477385115eb9e
Craig Andrews <craigandrews@nhs.net>
2016-06-10 16:12:57 +0100

    0033 digit cancelling fractions

---
+++ p0034_digit_cancelling_fractions_test.py
@@ -0,0 +1,39 @@
+"""
+The fraction 49/98 is a curious fraction, as an inexperienced mathematician in attempting to simplify it may incorrectly believe that 49/98 = 4/8, which is correct, is obtained by cancelling the 9s.
+
+We shall consider fractions like, 30/50 = 3/5, to be trivial examples.
+
+There are exactly four non-trivial examples of this type of fraction, less than one in value, and containing two digits in the numerator and denominator.
+
+If the product of these four fractions is given in its lowest common terms, find the value of the denominator.
+
+
+Answer:
+    100
+"""
+
+
+def nontrivial_fractions():
+    return ((x // 10, y % 10)
+            for x in xrange(10, 100)
+            for y in xrange((x % 10) * 10 + 1, (x % 10 + 1) * 10)
+            if y > x and
+               float(x) / float(y) == float(x // 10) / float(y % 10))
+
+
+def mul_fracs(fracs):
+    return reduce(lambda (a, b), (x, y): (a * x, b * y), fracs, (1, 1))
+
+
+def gcm(a, b):
+    return a if b == 0 else gcm(b, a % b)
+
+
+def digit_cancelling_fractions():
+    a, b = mul_fracs(nontrivial_fractions())
+    g = gcm(a, b)
+    return b / g
+
+
+def test_0034_digit_cancelling_fractions():
+    assert digit_cancelling_fractions() == 100
