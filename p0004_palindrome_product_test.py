3bac659be6f41d2e8065d7ca1420e02e99a4e54d
Craig Andrews <craig@lazyrobot.org.uk>
2016-06-23 17:27:52 +0100

    Gold plating problem 4

--- p0004_palindrome_product_test.py
+++ p0004_palindrome_product_test.py
@@ -8,28 +8,29 @@ Answer:
     906609
 """
 
-
 from euler.util import is_palindrome
 from itertools import takewhile
 
 
+def palindrome_products(start, end):
+    lower_bound = start
+
+    def countdown():
+        return takewhile(lambda n: n > lower_bound, range(end, 0, -1))
+
+    candidates = ((x * y, min(x, y), x, y)
+                  for x in countdown()
+                  for y in countdown()
+                  if is_palindrome(x * y))
+
+    for palindrome, new_lower_bound, x, y in candidates:
+        yield palindrome
+        lower_bound = new_lower_bound
+
+
 def max_palindrome_product(start, end):
-    def _palindrome_products():
-        lower_bound = start
-
-        def still_valid(n):
-            return n > lower_bound
-
-        for palindrome, new_lower_bound in ((x * y, min(x, y))
-                                            for x in takewhile(still_valid, range(end, 0, -1))
-                                            for y in takewhile(still_valid, range(end, 0, -1))
-                                            if is_palindrome(x * y)):
-            yield palindrome
-            lower_bound = new_lower_bound
-
-    return max(_palindrome_products())
+    return max(palindrome_products(start, end))
 
 
 def test_0004_palindrome_product():
     assert max_palindrome_product(100, 999) == 906609
-
