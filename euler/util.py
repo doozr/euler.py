7ec91ca99cd88ce1676bd591fb80d3b87d0575c5
Craig Andrews <craig@lazyrobot.org.uk>
2016-06-22 22:16:36 -0400

    Add handy benchmark functions

--- euler/util.py
+++ euler/util.py
@@ -1,5 +1,20 @@
+from time import time
+
+
 def is_palindrome(x):
     s = str(x)
     split = int(len(s) / 2 + 1)
     return s[:split] == s[-split:][::-1]
 
+
+def ms(t=None):
+    if not t:
+        t = time()
+    return int(t * 1000)
+
+
+def ms_timer(fn):
+    s = ms()
+    fn()
+    e = ms()
+    return e - s
